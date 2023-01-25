5c3ab5a5a1e9e26280dd16942ddd66a9749fc209
DyoMarselo <109974642+DyoDevLmao@users.noreply.github.com>
2023-01-25 13:48:08 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,55 +1,60 @@
 const fs = require('fs')
 const chalk = require('chalk')
-const  { indonesia, english, spanyol} = require(`./language`)
+const {
+   indonesia,
+   english,
+   spanyol
+} = require(`./language`)
 
 // Website Api (jgn di ganti tomlol)
 global.APIs = {
-	alfa: 'https://alphabot-api.herokuapp.com', //apabila link api eror, segera laporkan ke owner
+   alfa: 'https://api.zeeoneofc.my.id', //apabila link api eror, segera laporkan ke owner
 }
 
 // Free apikey (Apikey expired silahkan login terus ganti V12 dgn apikey lu)
 global.APIKeys = {
-	'https://alphabot-api.herokuapp.com': 'V12',  // 👉 login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
+   'https://api.zeeoneofc.my.id': '7DNeoKcX', // 👉 login https://api.zeeoneofc.xyz to get apikey
 }
 
 // setting 
-global.autoread = false // auto read message
-global.autobio = false 
-global.userRegister = false 
+global.autoread = true // auto read message
+global.autobio = false //auto ganti bio
+global.anticall = true //anti call 
+global.userRegister = true
 global.autoblok212 = true //ubah jadi true biar nmr 212 di blok sama bot
 
 //language
 //Available in indonesia , english & spanyol 
 //You can request others language 
-global.language = indonesia  //change indonesia to english if you don't understand the language used by the bot
+global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
 
 
 // Other
 // Other
-global.botname = "MIKU-BOT" //namabot kalian
-global.ownername= "RYU" //nama kalian
-global.myweb ="https://" //bebas asal jan hapus
-global.youtube = "https://" //bebas asal jan hapus
+global.botname = "MIKU-BOT MD" //namabot kalian
+global.ownername = "MIKU-BOT MD" //nama kalian
+global.myweb = "https://" //bebas asal jan hapus
+global.youtube = "https://youtube.com/@DyoMarselo" //bebas asal jan hapus
 global.myweb2 = "https://" //bebas
 global.email = "dyomarselo@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['6281228496899'] //premium user
-global.owner = ["6281228496899"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "6281228496899" // nomor wa kalian
-global.ownernomerr = "6281228496899" //nmr wa kalian
-global.thumbnail = "./image/Screenshot_2022-08-12-20-36-45-38.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
-global.donasi = "./image/qris_dinamis_1655141087404.png" // foto donasi di folder image
-global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
+global.premium = ['6281118496999'] //premium user
+global.owner = ["6281228496899", "6281228496999"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6281228496999" // nomor wa kalian
+global.ownernomerr = "+6281228496899" //nmr wa kalian
+global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.donasi = "./image/donasi.jpg" // foto donasi di folder image
+global.background_welcome = "https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
 global.packname = '© MIKU-BOT' //sticker wm ubah
-global.author = 'Di Buat Oleh MIKUBOT' //sticker wm ganti nama kalian
+global.author = 'Di Buat Oleh MIKU-BOT' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
 global.limitawal = {
-    premium: "10000",
-    free: 100
+   premium: "10000",
+   free: 100
 }
 
 global.adventureRPG = 'https://telegra.ph/file/ff94536d69e0f4f3e7b54.jpg'
@@ -67,46 +72,46 @@ global.mancingRPG = 'https://telegra.ph/file/1420d26b7f04f84993225.jpg'
 global.monthlyRPG = 'https://telegra.ph/file/7eadf65b95d9f7a3ecb46.jpg'
 global.weeklyRPG = 'https://telegra.ph/file/ffc85895d14db97931f0f.jpg'
 global.rpg = {
-  emoticon(string) {
-    string = string.toLowerCase()
-    let emot = {
-      level: '📊',
-      limit: '🎫',
-      health: '❤️',
-      exp: '✨',
-      money: '💹',
-      bank: '🏦',
-      potion: '🥤',
-      diamond: '💎',
-      common: '📦',
-      uncommon: '🛍️',
-      mythic: '🎁',
-      legendary: '🗃️',
-      superior: '💼',
-      pet: '🔖',
-      trash: '🗑',
-      armor: '🥼',
-      sword: '⚔️',
-      pickaxe: '⛏️',
-      fishingrod: '🎣',
-      wood: '🪵',
-      rock: '🪨',
-      string: '🕸️',
-      horse: '🐴',
-      cat: '🐱',
-      dog: '🐶',
-      fox: '🦊',
-      petFood: '🍖',
-      iron: '⛓️',
-      gold: '🪙',
-      emerald: '❇️',
-      upgrader: '🧰'
-      
-    }
-    let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
-    if (!results.length) return ''
-    else return emot[results[0][0]]
-  }
+   emoticon(string) {
+      string = string.toLowerCase()
+      let emot = {
+         level: '📊',
+         limit: '🎫',
+         health: '❤️',
+         exp: '✨',
+         money: '💹',
+         bank: '🏦',
+         potion: '🥤',
+         diamond: '💎',
+         common: '📦',
+         uncommon: '🛍️',
+         mythic: '🎁',
+         legendary: '🗃️',
+         superior: '💼',
+         pet: '🔖',
+         trash: '🗑',
+         armor: '🥼',
+         sword: '⚔️',
+         pickaxe: '⛏️',
+         fishingrod: '🎣',
+         wood: '🪵',
+         rock: '🪨',
+         string: '🕸️',
+         horse: '🐴',
+         cat: '🐱',
+         dog: '🐶',
+         fox: '🦊',
+         petFood: '🍖',
+         iron: '⛓️',
+         gold: '🪙',
+         emerald: '❇️',
+         upgrader: '🧰'
+
+      }
+      let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+      if (!results.length) return ''
+      else return emot[results[0][0]]
+   }
 }
 
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
@@ -114,26 +119,26 @@ global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutpu
 global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['', '!', '.', '🐦', '🐤', '🗿']
 global.sp = '⭔'
 global.mess = {
-    sukses: '🤗Done, Oke Desu~',
-    admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
-    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
-    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
-    group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
-    private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
-	bot: 'Fitur Khusus Pengguna Nomor Bot',
-    errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
-    wait: '⏳ Sedang Di Proses',
-	lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
-	example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
+   sukses: '🤗Done, Oke Desu~',
+   admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
+   botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
+   owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
+   group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
+   private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
+   bot: 'Fitur Khusus Pengguna Nomor Bot',
+   errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
+   wait: '⏳ Loading....',
+   lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
+   example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
 }
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
-	fs.unwatchFile(file)
-	console.log(chalk.redBright(`Update'${__filename}'`))
-	delete require.cache[file]
-	require(file)
+   fs.unwatchFile(file)
+   console.log(chalk.redBright(`Update'${__filename}'`))
+   delete require.cache[file]
+   require(file)
 })
